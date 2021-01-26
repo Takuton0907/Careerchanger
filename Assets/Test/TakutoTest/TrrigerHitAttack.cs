ac290357414c84721407d26f38200979f0c64690
Takuto0907 <syuukurasuta@ezweb.ne.jp>
2021-01-27 01:38:34 +0900

    ダメージを与える機能とダメージを受ける機能を作った

---
+++ Assets/Test/TakutoTest/TrrigerHitAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrrigerHitAttack : MonoBehaviour
+{
+    [SerializeField] int m_attackPower = 1;
+
+    /// <summary> 攻撃する親のオブジェクトを設定　空欄であれば全員に当たる </summary>
+    [SerializeField] Transform m_parentObj = null;
+
+    public void SetParentObj(Transform parent)
+    {
+        m_parentObj = parent;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out IDamageHandler damage))
+        {
+            if (collision.transform == m_parentObj) return;
+            damage.Damage(m_attackPower);
+            Debug.Log($"{transform.name} が {collision.transform.name} に {m_attackPower} 与えた");
+        }
+    }
+}
