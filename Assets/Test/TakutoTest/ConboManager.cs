58c9d90883c3415babe704a3953ab8551c76574d
IshiiTakto <syuukurasuta@ezweb.ne.jp>
2021-01-30 20:17:27 +0900

    コンボの下準備をしました

---
+++ Assets/Test/TakutoTest/ConboManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConboManager : MonoBehaviour
+{
+    [SerializeField] List<Combo> m_combos;
+}
+
+[Serializable]
+class Combo
+{
+    public string name;
+    public AttackMode[] combos = new AttackMode[3];
+}
