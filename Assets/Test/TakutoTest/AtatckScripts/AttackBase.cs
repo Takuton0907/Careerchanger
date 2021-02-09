66d86c4374a55e8870472493dbebd7472741d242
Takuto0907 <syuukurasuta@ezweb.ne.jp>
2021-02-09 19:03:04 +0900

    フォルダ整理をしました

---
+++ Assets/Test/TakutoTest/AtatckScripts/AttackBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AttackBase : MonoBehaviour
+{
+    [SerializeField] float m_recastTime = 1f;
+
+    protected BoxCollider2D m_collider = null;
+    abstract protected IEnumerator AttackCoroutine();
+    public void Attack()
+    {
+        StartCoroutine(AttackCoroutine());
+    }
+    protected void Start()
+    {
+        m_collider = GetComponent<BoxCollider2D>();
+    }
+
+    public float GetRecastTime() => m_recastTime;
+}
