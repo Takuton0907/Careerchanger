dd43391b26a92f2988f6ceed02a5010d28eb504f
Takuto0907 <syuukurasuta@ezweb.ne.jp>
2021-02-13 00:32:24 +0900

    タイトルのアニメ―ションを作りました

---
+++ Assets/Test/TakutoTest/Title/TitleTouch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleTouch : MonoBehaviour
+{
+    Button m_button = null;
+
+    private void Start()
+    {
+        m_button = GetComponent<Button>();
+    }
+
+    public void OnclickTouch()
+    {
+        m_button.interactable = false;
+    }
+}
--- Assets/script/TitleManager.cs
+++ Assets/script/TitleManager.cs
@@ -3,9 +3,16 @@
 public class TitleManager : MonoBehaviour
 {
     [SerializeField] float fadeInterval = 2;
-    
+
+    [SerializeField] Animator m_animator = null;
+
     public void OnClickGamaStart()
     {
         FadeManager.Instance.LoadScene("StageSelect", fadeInterval);
+    }
+
+    public void OnClickTouch()
+    {
+        m_animator?.SetTrigger("Tocuh");
     }
 }
