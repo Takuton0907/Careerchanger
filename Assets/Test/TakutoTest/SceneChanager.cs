a14bc59283304cf808908d6da4fe8896486a6e46
Takuto0907 <syuukurasuta@ezweb.ne.jp>
2021-02-16 05:12:10 +0900

    シーンを切り替えるだけの機能を作りました

---
+++ Assets/Test/TakutoTest/SceneChanager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanager : MonoBehaviour
+{
+    [SerializeField]
+    SceneNames m_sceneNames = SceneNames.Title;
+    [SerializeField]
+    float fadetime = 2;
+
+    public void SceneLoad()
+    {
+        FadeManager.Instance.LoadScene(m_sceneNames.ToString(), fadetime);
+    }
+}
+
+enum SceneNames
+{
+    Title,
+    StageSelect,
+    WeaponSelect,
+    Game,
+}
