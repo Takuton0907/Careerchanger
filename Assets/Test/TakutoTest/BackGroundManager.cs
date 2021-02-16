bb6936d5923167bccaa22e6e37885b4416d7de0f
Takuto0907 <syuukurasuta@ezweb.ne.jp>
2021-02-17 04:09:18 +0900

    背景が途中から見切れてしまうバグを修正しました

--- Assets/Test/TakutoTest/BackGroundManager.cs
+++ Assets/Test/TakutoTest/BackGroundManager.cs
@@ -10,16 +10,12 @@ public class BackGroundManager : MonoBehaviour
 
     [Header("雲の動くスピード")]
     [SerializeField] float m_cloudSpeed = -1.3f;
-    [SerializeField] float m_playerSpeedAdd = 0.2f;
 
-    Transform[] m_repeatedlyImages = new Transform[2];
+    Transform[] m_repeatedlyImages = new Transform[3];
 
     float m_cloudRetunePosiX = 20;
     /// <summary> プレイヤーのTransform </summary>
     Transform m_playerTrans = default;
-
-    /// <summary> ゴールのポジション </summary>
-    Vector3 m_clearPosi = default;
 
     private void Awake()
     {
@@ -28,11 +24,9 @@ public class BackGroundManager : MonoBehaviour
 
     private void Start()
     {
-        m_repeatedlyImages[0] = m_cloud.transform;
-        m_repeatedlyImages[1] = DuplicationSprite(m_cloud, ref m_cloudRetunePosiX).transform;
+        m_repeatedlyImages = DuplicatBackGrounds(m_cloud, 3);
 
         m_playerTrans = LevelManager.Instance.PlayerCon.transform;
-        m_clearPosi = FindObjectOfType<ClearObject>().transform.position;
 
         RepeatedlyMove();
     }
@@ -42,8 +36,8 @@ public class BackGroundManager : MonoBehaviour
     {
         for (int i = 0; i < m_repeatedlyImages.Length; i++)
         {
-            m_repeatedlyImages[i].Translate(m_cloudSpeed * Time.deltaTime, 0, 0); 
-            
+            m_repeatedlyImages[i].Translate(m_cloudSpeed * Time.deltaTime, 0, 0);
+
             if (m_playerTrans.position.x - m_repeatedlyImages[i].position.x > m_cloudRetunePosiX)
             {
                 int index = i - 1 >= 0 ? i - 1 : m_repeatedlyImages.Length - 1;
@@ -54,16 +48,29 @@ public class BackGroundManager : MonoBehaviour
     }
 
     /// <summary> 画像を複製して横に並べます </summary>
-    private GameObject DuplicationSprite(GameObject backGround, ref float retunePosi)
+    private GameObject DuplicationSprite(GameObject backGround, float space)
     {
-        if (backGround.TryGetComponent(out SpriteRenderer s))
+        Vector3 instancePosi = new Vector3(backGround.transform.position.x + space, backGround.transform.position.y, backGround.transform.position.z);
+        GameObject obj = Instantiate(backGround, instancePosi, Quaternion.identity, backGround.transform.parent);
+        return obj;
+    }
+
+    private Transform[] DuplicatBackGrounds(GameObject obje, int num)
+    {
+        Transform[] transforms = new Transform[num];
+
+        if (obje.TryGetComponent(out SpriteRenderer s))//スプライトの場合の計算
         {
-            retunePosi = backGround.transform.position.x + Mathf.Floor(s.size.x) * backGround.transform.localScale.x;
-            
-            Vector3 instancePosi = new Vector3(backGround.transform.position.x + retunePosi, backGround.transform.position.y, backGround.transform.position.z);
-            GameObject obj = Instantiate(backGround, instancePosi, Quaternion.identity, backGround.transform.parent);
-            return obj;
+            m_cloudRetunePosiX = obje.transform.position.x + Mathf.Floor(s.size.x) * obje.transform.localScale.x;
         }
-        return null;
+
+
+        transforms[0] = obje.transform;
+        for (int i = 1; i < transforms.Length; i++)
+        {
+            transforms[i] = DuplicationSprite(transforms[i - 1].gameObject, m_cloudRetunePosiX).transform;
+        }
+
+        return transforms;
     }
 }
