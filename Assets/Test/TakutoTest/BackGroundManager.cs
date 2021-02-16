63fcaf638c011973174ca1939ef3da6b7eda6e49
Takuto0907 <syuukurasuta@ezweb.ne.jp>
2021-02-17 01:33:42 +0900

    背景の移動に違和感があったので修正しました

--- Assets/Test/TakutoTest/BackGroundManager.cs
+++ Assets/Test/TakutoTest/BackGroundManager.cs
@@ -6,35 +6,20 @@ using UnityEngine;
 public class BackGroundManager : MonoBehaviour
 {
     [Header("背景画像")]
-    ////[SerializeField] GameObject m_sky = null;
     [SerializeField] GameObject m_cloud = null;
-    //[SerializeField] GameObject m_middle = null; 
-    //[SerializeField] GameObject m_front = null;
 
     [Header("雲の動くスピード")]
     [SerializeField] float m_cloudSpeed = -1.3f;
     [SerializeField] float m_playerSpeedAdd = 0.2f;
-
-    //[Header("クリア時までの背景調整")]
-    //[SerializeField] float m_clearOffset = 30;
 
     Transform[] m_repeatedlyImages = new Transform[2];
 
     float m_cloudRetunePosiX = 20;
     /// <summary> プレイヤーのTransform </summary>
     Transform m_playerTrans = default;
-    ///// <summary> プレイヤーのRigidbody </summary>
-    //Rigidbody2D m_playerRig = default;
-    ///// <summary> ステージ全体の長さ </summary>
-    //float m_stageDistance = default;
-    ///// <summary> スタートのポジション </summary>
-    //Vector3 m_startPosi = default;
+
     /// <summary> ゴールのポジション </summary>
     Vector3 m_clearPosi = default;
-
-    //Vector3 m_middleStartPosi;
-    //Vector3 m_frontStartPosi;
-    //Vector3 m_skyStartPosi;
 
     private void Awake()
     {
@@ -46,61 +31,27 @@ public class BackGroundManager : MonoBehaviour
         m_repeatedlyImages[0] = m_cloud.transform;
         m_repeatedlyImages[1] = DuplicationSprite(m_cloud, ref m_cloudRetunePosiX).transform;
 
-        //m_middleStartPosi = m_middle.transform.position;
-        //m_frontStartPosi = m_front.transform.position;
-        //m_skyStartPosi = m_sky.transform.position;
-
         m_playerTrans = LevelManager.Instance.PlayerCon.transform;
-        //m_startPosi = m_playerTrans.position;
         m_clearPosi = FindObjectOfType<ClearObject>().transform.position;
 
-        //m_stageDistance = m_clearPosi.x - m_playerTrans.position.x;
-
-        //m_playerRig = LevelManager.Instance.PlayerCon.GetComponent<Rigidbody2D>();
-
-        //m_clearOffset = m_middle.GetComponent<SpriteRenderer>().size.x * m_middle.transform.localScale.x / 2 - m_clearOffset;
-
-        //BackgroundMove();
         RepeatedlyMove();
     }
 
     /// <summary> 画像を一定のスピードで動かし続けます </summary>
     public void RepeatedlyMove()
     {
-        foreach (var cloud in m_repeatedlyImages)
+        for (int i = 0; i < m_repeatedlyImages.Length; i++)
         {
-            cloud.Translate(m_cloudSpeed * Time.deltaTime, 0, 0);
+            m_repeatedlyImages[i].Translate(m_cloudSpeed * Time.deltaTime, 0, 0); 
             
-            if (cloud.localPosition.x < -m_cloudRetunePosiX)
+            if (m_playerTrans.position.x - m_repeatedlyImages[i].position.x > m_cloudRetunePosiX)
             {
-                cloud.localPosition = new Vector3(m_cloudRetunePosiX - 0.1f, 0, cloud.position.z);
+                int index = i - 1 >= 0 ? i - 1 : m_repeatedlyImages.Length - 1;
+                float nextPositionX = m_repeatedlyImages[index].position.x + m_cloudRetunePosiX;
+                m_repeatedlyImages[i].transform.position = new Vector3(nextPositionX - 0.1f, 0, m_repeatedlyImages[i].position.z);
             }
         }
     }
-
-    ///// <summary> 三枚目以外を動かします </summary>
-    //public void BackgroundMove()
-    //{
-    //    float playerProgress = (m_playerTrans.position.x - m_startPosi.x) / m_stageDistance;
-
-    //    Vector3 start = m_startPosi + new Vector3(m_clearOffset, 0, 0);
-    //    Vector3 gool = m_clearPosi - new Vector3(m_clearOffset / 5, 0, 0);
-
-    //    Vector3 posi = Vector3.Lerp(start, gool, playerProgress);
-    //    posi.z = m_middleStartPosi.z;
-    //    posi.y = m_middleStartPosi.y;
-    //    m_middle.transform.position = posi;
-
-    //    posi = Vector3.Lerp(start, gool, playerProgress);
-    //    posi.z = m_frontStartPosi.z;
-    //    posi.y = m_frontStartPosi.y;
-    //    m_front.transform.position = posi;
-
-    //    posi = Vector3.Lerp(start, gool, playerProgress);
-    //    posi.z = m_skyStartPosi.z;
-    //    posi.y = m_skyStartPosi.y;
-    //    m_sky.transform.position = posi;
-    //}
 
     /// <summary> 画像を複製して横に並べます </summary>
     private GameObject DuplicationSprite(GameObject backGround, ref float retunePosi)
@@ -108,9 +59,7 @@ public class BackGroundManager : MonoBehaviour
         if (backGround.TryGetComponent(out SpriteRenderer s))
         {
             retunePosi = backGround.transform.position.x + Mathf.Floor(s.size.x) * backGround.transform.localScale.x;
-            Debug.Log($"posi = {backGround.transform.position.x}s.size.x = {s.size.x} size = {backGround.transform.localScale.x}");
-
-
+            
             Vector3 instancePosi = new Vector3(backGround.transform.position.x + retunePosi, backGround.transform.position.y, backGround.transform.position.z);
             GameObject obj = Instantiate(backGround, instancePosi, Quaternion.identity, backGround.transform.parent);
             return obj;
